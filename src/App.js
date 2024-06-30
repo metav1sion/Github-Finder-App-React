fa6dd78c5e14f894fd55917afc89f21b873062a2
Metehan Sözen <metehansozen@outlook.com>
2024-06-30 20:32:12 +0300

    kullanıcı arama kısmı tamamlandı, alert kısmı yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Navbar from './components/Navbar'
 import UserList from "./components/UserList";
+import Search from "./components/Search";
+import Alert from "./components/Alert";
+
 
 // HTTP Request componentdidmount
 
@@ -10,24 +13,57 @@ class App extends Component {
         super(props);
         this.state = {
             loading: false,
-            users:  []
+            users:  [],
+            showClearButton: false,
+            alert: null
         }
     }
 
-    componentDidMount() {
-        this.setState({loading: true});
+    searchUsers = (value) => {
+        let newValue = value.trim()
 
+        this.setState({loading: true});
         setTimeout(()=>{
-            fetch("https://api.github.com/users")
+            fetch("https://api.github.com/search/users?q=" + newValue)
                 .then(response => response.json())
-                .then(data => this.setState({users: data, loading: false}))
-        },2000)
+                .then(data => this.setState({users: data.items, loading: false}))
+        },1000)
+        this.setState({showClearButton: true})
+
+
+    }
+
+    // componentDidMount() {
+    //     this.setState({loading: true});
+    //
+    //     setTimeout(()=>{
+    //         fetch("https://api.github.com/users")
+    //             .then(response => response.json())
+    //             .then(data => this.setState({users: data, loading: false}))
+    //     },2000)
+    //}
+
+    deleteUsers = () => {
+        this.setState({users: [], showClearButton: false });
+    }
+
+    displayAlert = (msg,type) => {
+        this.setState({
+            alert: {msg: msg, type: type}
+        });
+        setTimeout(() => {
+            this.setState({
+                alert: null
+            });
+        },3000)
     }
 
     render() {
         return (
             <div>
                 <Navbar/>
+                <Search displayAlert={this.displayAlert} showClearButton={this.state.showClearButton} search={this.searchUsers} delete={this.deleteUsers}/>
+                <Alert error={this.state.alert}/>
                 <div className={"container mt-3"}>
                     <UserList users = {this.state.users} loading={this.state.loading}/>
                 </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,51 @@
 import React, {Component} from 'react';
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            value: ""
+        }
+    }
+
+    onChange = (event) => {
+        //console.log(event.target.value);
+        this.setState({
+            value: event.target.value
+        })
+    }
+    onSubmit = (event) => {
+        event.preventDefault();//sayfa yenilenmesini önlüyor.
+        let newValue = this.state.value.trim();
+        if (newValue === "") {
+            this.props.displayAlert("Lütfen bir değer giriniz.", "danger")
+        }
+        else {
+            this.props.search(this.state.value)
+            this.setState({value: ""})
+        }
+
+    }
+
+    // deleteUsers = () => {
+    //     this.props.delete();
+    // }
+
     render() {
         return (
             <div className={"container my-3"}>
+                <form onSubmit={this.onSubmit}>
+                    <div className="input-group">
+                        <input value={this.state.value} onChange={this.onChange} type="text" className="form-control" placeholder="Ara..."/>
+                        <button className={"btn btn-primary"} type={"submit"} >Ara</button>
+                    </div>
+                </form>
+
+                {
+                    this.props.showClearButton &&
+                    <button className={"btn btn-outline-danger mt-2 btn-block"} onClick={this.props.delete}>Sonuçları
+                        Temizle</button>
+                }
 
             </div>
         );
